09b34b397539763853ff38141b0ab76eb303cfbf
Hua Son Tung <tunghuason@gmail.com>
2022-03-22 08:00:30 +0700

    Set IBOutlet and IBAction for labels and buttons
    Set IBOutlet for working and result label; Set IBAction for basic operators and numbers button

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var calculatorWorking: UILabel!
+    @IBOutlet weak var calculatorResult: UILabel!
+    
+    var workingSection = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func allClearTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func backSpaceTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func percentTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func divideTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func multiplyTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func minusTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func plusTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func euqalTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func numberTapped(_ sender: Any) {
+        
+    }
 }
 
