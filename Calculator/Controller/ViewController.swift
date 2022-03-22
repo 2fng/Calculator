95744fd3a384260b3edd6dc398140236bbcd0c14
Hua Son Tung <tunghuason@gmail.com>
2022-03-22 10:18:35 +0700

    Change operator's icon; Finish equal function and add checking and format result function

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var calculatorWorking: UILabel!
     @IBOutlet weak var calculatorResult: UILabel!
     
-    var workingSection = "9 + 8"
+    var workingSection = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,33 +39,84 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func percentTapped(_ sender: Any) {
-        
-    }
-    
-    @IBAction func divideTapped(_ sender: Any) {
-        
-    }
-    
-    @IBAction func multiplyTapped(_ sender: Any) {
-        
-    }
-    
-    @IBAction func minusTapped(_ sender: Any) {
-        
-    }
-    
-    @IBAction func plusTapped(_ sender: Any) {
-        
+    @IBAction func operatorTapped(_ sender: UIButton) {
+        let inputOperator = sender.titleLabel?.text
+        workingSection = workingSection + inputOperator!
+        calculatorWorking.text = workingSection
     }
     
     @IBAction func euqalTapped(_ sender: Any) {
         
+        if validInput() {
+            let checkedWorkingForPercent = workingSection.replacingOccurrences(of: "%", with: "*0.01")
+            let expression = NSExpression(format: checkedWorkingForPercent)
+            let result = expression.expressionValue(with: nil, context: nil) as! Double
+            let resultString = formatResult(result: result)
+            calculatorResult.text = resultString
+        } else {
+            let alert = UIAlertController(title: "Invalid Input", message: "Syntax Error", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func validInput() -> Bool {
+        
+        var count = 0
+        var funcCharIndexes = [Int]()
+        
+        for char in workingSection {
+            if isSpecialCharacter(char: char) {
+                funcCharIndexes.append(count)
+            }
+            count += 1
+        }
+        
+        if workingSection.isEmpty {
+            return false
+        }
+        
+        var previousChar: Int = -1
+        
+        for index in funcCharIndexes {
+            if index == 0 {
+                return false
+            }
+            
+            if index == workingSection.count - 1 {
+                return false
+            }
+            
+            if previousChar != -1 {
+                
+                if index - previousChar == 1 {
+                    return false
+                }
+            }
+            previousChar = index
+            
+        }
+        
+        return true
+    }
+    
+    func isSpecialCharacter(char: Character) -> Bool {
+        if (char == "%" || char == "/" || char == "*" || char == "-" || char == "+" || char == ".") {
+            return true
+        }
+        
+        return false
+    }
+    
+    func formatResult(result: Double) -> String {
+            return String(format: "%.2f", result)
     }
     
     //MARK: Number's IBAction
-    @IBAction func numberTapped(_ sender: Any) {
-        
+    @IBAction func numberTapped(_ sender: UIButton) {
+        let temp = sender.titleLabel?.text
+        workingSection = workingSection + temp!
+        calculatorWorking.text = workingSection
     }
 }
 
